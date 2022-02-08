b566573223b8a6570b725988212de73c080a2f0a
christianvegag <christianvegaguizabalo@gmail.com>
2022-02-08 18:23:08 -0500

    Actualizacion con Boostrap

--- assets/js/api.js
+++ assets/js/api.js
@@ -33,16 +33,49 @@ const fillData = (data) => {
 const fillDataImg = (dataImg, img) => {
 
 
-  html += '<div class="col">';
-  html += '<div class="card h-100 bg-info bg-opacity-10">';
-  html += `<img src="${img.front_default}" class="card-img-top" style="height:250px;" alt="...">`;
-  html += '<div class="card-body text-center">';
-  html += `<h3 class="card-title text-uppercase">${dataImg.name}</h3>`;
-  html += `<h5 class="card-text">Altura -> ${dataImg.height}</h5>`;
-  html += `<h5 class="card-text">Peso -> ${dataImg.weight}</h5>`;
-  html += "</div>";
-  html += "</div>";
-  html += "</div>";
+  // html += '<div class="col">';
+  // html += '<div class="card h-100 bg-info bg-opacity-10">';
+  // html += `<img src="${img.front_default}" class="card-img-top" style="height:250px;" alt="...">`;
+  // html += '<div class="card-body text-center">';
+  // html += `<h3 class="card-title text-uppercase">${dataImg.name}</h3>`;
+  // html += `<h5 class="card-text">Altura -> ${dataImg.height}</h5>`;
+  // html += `<h5 class="card-text">Peso -> ${dataImg.weight}</h5>`;
+  // html += "</div>";
+  // html += "</div>";
+  // html += "</div>";
+
+  html += `
+  <div class="wrapper">
+        <div class="clash-card barbarian">
+          <div class="clash-card__image clash-card__image--barbarian">
+            <img src="${img.front_default}"  style="height:280px;" alt="..." alt="barbarian" />
+          </div>
+          <div class="clash-card__level clash-card__level--barbarian">Level ${dataImg.base_experience}</div>
+          <div class="clash-card__unit-name text-uppercase">${dataImg.name}</div>
+          <div class="clash-card__unit-description">
+            The ${dataImg.name} it's a special pokemon so you can't trade it.
+          </div>
+    
+          <div class="clash-card__unit-stats clash-card__unit-stats--barbarian clearfix">
+            <div class="one-third">
+              <div class="stat">${dataImg.height}</div>
+              <div class="stat-value">Height</div>
+            </div>
+    
+            <div class="one-third">
+              <div class="stat">${dataImg.weight}</div>
+              <div class="stat-value">Weight</div>
+            </div>
+    
+            <div class="one-third no-border">
+              <div class="stat">${dataImg.base_experience}</div>
+              <div class="stat-value">Power</div>
+            </div>
+    
+          </div>
+    
+        </div> <!-- end clash-card barbarian-->
+      </div> <!-- end wrapper -->`
 
   document.getElementById("characters").innerHTML = html;
 };
@@ -51,8 +84,8 @@ const fillDataImg = (dataImg, img) => {
 const pagination = (info) => {
   let html = "";
 
-  html += `<li style="cursor:pointer;" class="page-item ${info.previous == null ? "disabled" : ""}"><a class="page-link" onclick="getAPI('${info.previous}')">Prev</a></li>`;
-  html += `<li style="cursor:pointer;" class="page-item ${info.next == null ? "disabled" : ""}"><a class="page-link" onclick="getAPI('${info.next}')">Next</a></li>`;
+  html += `<li style="cursor:pointer;" class="page-item ${info.previous == null ? "disabled" : ""}"><a class="page-link bg-transparent" onclick="getAPI('${info.previous}')">Prev</a></li>`;
+  html += `<li style="cursor:pointer;" class="page-item ${info.next == null ? "disabled" : ""}"><a class="page-link bg-transparent" onclick="getAPI('${info.next}')">Next</a></li>`;
 
   document.getElementById("pagination").innerHTML = html;
 };
